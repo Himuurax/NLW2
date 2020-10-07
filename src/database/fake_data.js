e30d1a6155e6df8939ac52a7b9458aaff21276d6
Lucas Dominguez <luhcas_@live.com>
2020-10-07 19:36:48 -0300

    Initial commit

---
+++ src/database/fake_data.js
@@ -0,0 +1,35 @@
+const proffys = [
+    { name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp:"89987654534",
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+     cost: "20", 
+     weekday:[0], 
+     time_from:[720], 
+     time_to:[1220],
+    },
+    { 
+    name: "Daniele Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp:"89987654534",
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+     cost: "20", 
+     weekday:[1], 
+     time_from:[720], 
+     time_to:[1220],
+    },
+    { 
+    name: "Lucas Dominguez", 
+    avatar: "https://avatars0.githubusercontent.com/u/61797936?s=460&u=ceb4c7f8c90a4218cae4d53f1a4fb44cf6f142f9&v=4", 
+    whatsapp:"89987654534",
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+     cost: "20", 
+     weekday:[1], 
+     time_from:[720], 
+     time_to:[1220],
+    }
+    
+]
